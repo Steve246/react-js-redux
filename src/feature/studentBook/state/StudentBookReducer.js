a45c4d72c88462227826ff3ba924e996122bc488
Steve Jo <tedjasuryastevenjonathan@gmail.com>
2022-08-03 14:37:59 +0700

    [ADD] Dibikin gak spread

--- src/feature/studentBook/state/StudentBookReducer.js
+++ src/feature/studentBook/state/StudentBookReducer.js
@@ -10,7 +10,8 @@ export function studentBookReducer(state = bookInitialState, action) {
     case STUDENT_ADD_BOOK:
       return Object.assign({}, state, {
         student: action.payload.student,
-        books: [...state.books, ...action.payload.book],
+        books: [...state.books, action.payload.book],
+        // books: [...state.books, ...action.payload.book], --> bikin ini biar kebuang spreadnya
       });
     default:
       return state;
