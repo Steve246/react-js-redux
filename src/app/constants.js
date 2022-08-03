f3262d48d86da771623634648352a54008a9b612
Steve Jo <tedjasuryastevenjonathan@gmail.com>
2022-08-03 11:59:49 +0700

    [ADD] StudentBook

--- src/app/constants.js
+++ src/app/constants.js
@@ -2,4 +2,6 @@ export const STUDENT_ADD_AGE = "STUDENT_ADD_AGE";
 
 export const STUDENT_CHANGE_NAME = "STUDENT_CHANGE_NAME";
 
-export const PLUS = "PLUS";
+// export const PLUS = "PLUS";
+
+export const STUDENT_ADD_BOOK = "STUDENT_ADD_BOOK";
--- src/app/rooterReducer.js
+++ src/app/rooterReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { studentReducer } from "../feature/student/state/StudentReducer";
+import { studentBookReducer } from "../feature/studentBook/state/StudentBookReducer";
 
 export function rootReducer() {
   return combineReducers({
     studentReducer,
+    bookReducer: studentBookReducer,
   });
 }
